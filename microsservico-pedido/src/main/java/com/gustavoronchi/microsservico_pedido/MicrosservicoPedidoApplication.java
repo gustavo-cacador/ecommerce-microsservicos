cfb6777b6355736eff7a849f0bce916c59c63dfb
Gustavo Caçador <gustavoirl456@outlook.com>
2026-08-18 22:22:09 -0300

    criando microsserviço de pedido

---
+++ microsservico-pedido/src/main/java/com/gustavoronchi/microsservico_pedido/MicrosservicoPedidoApplication.java
@@ -0,0 +1,13 @@
+package com.gustavoronchi.microsservico_pedido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrosservicoPedidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrosservicoPedidoApplication.class, args);
+	}
+
+}
---
+++ microsservico-pedido/src/test/java/com/gustavoronchi/microsservico_pedido/MicrosservicoPedidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavoronchi.microsservico_pedido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrosservicoPedidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
