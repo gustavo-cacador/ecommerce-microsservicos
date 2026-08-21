496d187a0eb2e80cb4d7f49d868bda31b1ee0a36
Gustavo Caçador <gustavoirl456@outlook.com>
2026-08-21 02:07:10 -0300

    adicionando mais opções no status do pedido

--- microsservico-pedido/src/main/java/com/gustavoronchi/microsservico_pedido/enums/StatusOrder.java
+++ microsservico-pedido/src/main/java/com/gustavoronchi/microsservico_pedido/enums/StatusOrder.java
@@ -3,6 +3,8 @@ package com.gustavoronchi.microsservico_pedido.enums;
 public enum StatusOrder {
     CREATED,
     WAITING_PAYMENT,
+    APPROVED_PAYMENT,
+    DECLINED_PAYMENT,
     PAID,
     SHIPPED,
     DELIVERED,
