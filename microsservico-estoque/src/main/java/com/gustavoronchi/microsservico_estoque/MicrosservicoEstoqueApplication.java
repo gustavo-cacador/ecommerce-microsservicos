19d73f69640dd01016c687f07a2979e78af73d37
Gustavo Caçador <gustavoirl456@outlook.com>
2026-09-01 21:54:15 -0300

    criando microsservico de estoque

---
+++ microsservico-estoque/src/main/java/com/gustavoronchi/microsservico_estoque/MicrosservicoEstoqueApplication.java
@@ -0,0 +1,13 @@
+package com.gustavoronchi.microsservico_estoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrosservicoEstoqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrosservicoEstoqueApplication.class, args);
+	}
+
+}
---
+++ microsservico-estoque/src/test/java/com/gustavoronchi/microsservico_estoque/MicrosservicoEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavoronchi.microsservico_estoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrosservicoEstoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
