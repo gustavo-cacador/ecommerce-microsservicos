462f5df96b14d941f4c1b08ea9578cfa1ffb9503
Gustavo Caçador <gustavoirl456@outlook.com>
2026-09-01 21:40:30 -0300

    removendo order ao toString

--- microsservico-pedido/src/main/java/com/gustavoronchi/microsservico_pedido/database/model/OrderItem.java
+++ microsservico-pedido/src/main/java/com/gustavoronchi/microsservico_pedido/database/model/OrderItem.java
@@ -86,7 +86,6 @@ public class OrderItem {
     public String toString() {
         return "OrderItem{" +
                 "id=" + id +
-                ", order=" + order +
                 ", productId=" + productId +
                 ", quantity=" + quantity +
                 ", price=" + price +
