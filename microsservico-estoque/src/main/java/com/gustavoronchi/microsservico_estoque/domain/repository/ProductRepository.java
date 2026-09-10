1075e6b30674bb689df645592a5f235012d46d19
Gustavo Caçador <gustavoirl456@outlook.com>
2026-09-09 23:54:59 -0300

    resolvendo problema com requisições simultaneas com Lock

--- microsservico-estoque/src/main/java/com/gustavoronchi/microsservico_estoque/domain/repository/ProductRepository.java
+++ microsservico-estoque/src/main/java/com/gustavoronchi/microsservico_estoque/domain/repository/ProductRepository.java
@@ -1,9 +1,22 @@
 package com.gustavoronchi.microsservico_estoque.domain.repository;
 
 import com.gustavoronchi.microsservico_estoque.domain.entities.Product;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ProductRepository extends JpaRepository<Product, UUID> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("""
+            select p
+            from Product p 
+            where p.id = :id
+             """)
+    Optional<Product> findByIdForUpdate(@Param("id") UUID id);
 }
