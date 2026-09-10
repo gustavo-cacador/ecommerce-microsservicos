8e903fea6603cb0a33ae5c7fa421eee0b772f019
Gustavo Caçador <gustavoirl456@outlook.com>
2026-09-10 00:00:34 -0300

    ajustando chamadas http sincronas para retornar lista com os dados dos produtos eliminando todas as chamadas gets subsequentes

---
+++ microsservico-estoque/src/main/java/com/gustavoronchi/microsservico_estoque/dto/ReservedItemDTO.java
@@ -0,0 +1,34 @@
+package com.gustavoronchi.microsservico_estoque.dto;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public class ReservedItemDTO {
+
+    private UUID productId;
+    private BigDecimal price;
+
+    public ReservedItemDTO() {
+    }
+
+    public ReservedItemDTO(UUID productId, BigDecimal price) {
+        this.productId = productId;
+        this.price = price;
+    }
+
+    public UUID getProductId() {
+        return productId;
+    }
+
+    public void setProductId(UUID productId) {
+        this.productId = productId;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
--- microsservico-estoque/src/main/java/com/gustavoronchi/microsservico_estoque/dto/StockItemResponseDTO.java
+++ microsservico-estoque/src/main/java/com/gustavoronchi/microsservico_estoque/dto/StockItemResponseDTO.java
@@ -1,20 +1,49 @@
 package com.gustavoronchi.microsservico_estoque.dto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StockItemResponseDTO {
 
     private boolean success;
     private String failureReason;
+    private List<ReservedItemDTO> items = new ArrayList<>();
 
     public StockItemResponseDTO() {
     }
+
     public StockItemResponseDTO(boolean success, String failureReason) {
         this.success = success;
         this.failureReason = failureReason;
     }
 
-    public boolean isSuccess() { return success; }
-    public void setSuccess(boolean success) { this.success = success; }
+    public StockItemResponseDTO(boolean success, String failureReason, List<ReservedItemDTO> items) {
+        this.success = success;
+        this.failureReason = failureReason;
+        this.items = items;
+    }
 
-    public String getFailureReason() { return failureReason; }
-    public void setFailureReason(String failureReason) { this.failureReason = failureReason; }
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getFailureReason() {
+        return failureReason;
+    }
+
+    public void setFailureReason(String failureReason) {
+        this.failureReason = failureReason;
+    }
+
+    public List<ReservedItemDTO> getItems() {
+        return items;
+    }
+
+    public void setItems(List<ReservedItemDTO> items) {
+        this.items = items;
+    }
 }
