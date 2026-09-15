57357c6700d7644159e6bc9456d3767e3caef64d
Gustavo Caçador <gustavoirl456@outlook.com>
2026-09-14 23:16:59 -0300

    criando microsserviço de pagamento

---
+++ microsservico-pagamento/src/test/java/com/gustavoronchi/microsservico_pagamento/MicrosservicoPagamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavoronchi.microsservico_pagamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrosservicoPagamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
